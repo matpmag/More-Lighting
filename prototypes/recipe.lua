eec7f70e1e23907c4d9c4f56407f19731ae63505
Matt Maguire <matpmag@gmail.com>
2020-03-29 14:43:10 +0100

    Underwater lighting main feature added

---
+++ prototypes/recipe.lua
@@ -0,0 +1,13 @@
+data:extend({
+	{ 
+		type = "recipe",
+		name = "underwater-lamp",
+		enabled = false,
+		ingredients =
+		{
+		  {"small-lamp", 1},
+		  {"plastic-bar", 2},
+		},
+		result = "underwater-lamp"
+	},
+})
